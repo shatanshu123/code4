37d599971b2930a7a7389509171df81e8e8c55f1
shatanshu123 <63258511+shatanshu123@users.noreply.github.com>
2020-05-12 13:22:25 +0530

    Create first_bad_version.cpp

---
+++ first_bad_version.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        int middle;
+        while(start <= end){
+            middle = ((end-start)>> 1) + start;
+            if(isBadVersion(middle)) end = middle - 1;
+            else start = middle + 1;
+        }
+        return end + 1;
+    }
+};
